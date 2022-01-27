4ad0a7ca57fb9adc4204aaa409496202674dd662
himel8 <himelkhan290@gmail.com>
2022-01-28 02:02:04 +0600

    added status api

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -58,6 +58,23 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+// update an order status by id
+router.put("/status/:id", async (req, res) => {
+  try {
+    const updateOrder = await Order.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          status: req.body,
+        },
+      }
+    );
+    res.status(200).json(updateOrder);
+  } catch (err) {
+    res.status(500).json("there was a server side error");
+  }
+});
+
 // Delete an order by id
 router.delete("/:id", async (req, res) => {
   try {
