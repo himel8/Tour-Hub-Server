78e1411522ee2be44106c6f92b975fb4938d4982
himel8 <himelkhan290@gmail.com>
2022-01-28 00:08:24 +0600

    added user api

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -23,7 +23,7 @@ router.get("/", async (req, res) => {
 router.get("/:email", async (req, res) => {
   try {
     const user = await User.find({ email: req.params.email });
-    res.status(200).json(user[0]);
+    res.status(200).json(user[0]?.isAdmin);
   } catch (err) {
     res.status(500).json({
       error: "there was a server side error",
